d38733320334ded237394439ce31d3fc55c1034e
Igor Trindade <igortrindademe@gmail.com>
2023-06-08 09:15:13 -0300

    feat: Update page title

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,12 @@ module.exports = defineConfig({
   outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production'
     ? 'https://igortrinidad.github.io/kanban-board-code-test/'
-    : '/'
+    : '/',
+
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'Kanban Board Code Test - By Igor Trindade',
+    }
+  }
 })
