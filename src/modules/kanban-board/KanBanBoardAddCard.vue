2a887a986fb7996e01247423fbb1ea02d766e82e
Igor Trindade <igortrindademe@gmail.com>
2023-06-08 10:06:56 -0300

    feat: Added addItem action

--- src/modules/kanban-board/KanBanBoardAddCard.vue
+++ src/modules/kanban-board/KanBanBoardAddCard.vue
@@ -5,9 +5,15 @@
       <button v-if="!addCard" class="btn btn-primary text-xs" @click="toggleAddCard()">Add Card</button>
   
       <div class="w-full flex flex-col space-y-2" v-else>
-        <textarea ref="textarea" type="text" class="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-sky-500" placeholder="Enter a title for this card"></textarea>
+        <textarea 
+          ref="textarea" 
+          type="text" 
+          class="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-sky-500" 
+          placeholder="Enter a title for this card"
+          v-model="content"
+        ></textarea>
         <div class="w-full flex space-x-2">
-          <button class="btn btn-primary text-xs" @click="toggleAddCard()">Add</button>
+          <button class="btn btn-primary text-xs" @click="addItem()" :disabled="content.length <= 2">Add</button>
           <button class="btn text-xs" @click="toggleAddCard()">Cancel</button>
         </div>
       </div>
@@ -16,10 +22,20 @@
 </template>
 
 <script>
+
+import { useKanbanBoardStore } from './store'
+
 export default {
+  props: {
+    columnIndex: {
+      type: Number,
+      required: true
+    }
+  },
   data() {
     return {
-      addCard: false
+      addCard: false,
+      content: ''
     }
   },
   methods: {
@@ -31,7 +47,15 @@ export default {
             this.$refs.textarea.focus()
           })
         }
-      }, 400)
+      }, 300)
+    },
+
+    addItem() {
+      this.toggleAddCard()
+      setTimeout(() => {
+        useKanbanBoardStore().addItem(this.columnIndex, this.content)
+        this.content = ''
+      }, 200)
     }
   }
 }
