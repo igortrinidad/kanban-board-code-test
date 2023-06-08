783ad1074c8052e55d4aa4a5d706d2ab525f75dc
Igor Trindade <igortrindademe@gmail.com>
2023-06-08 08:59:45 -0300

    fix: Added github public path for assets

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'docs'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://igortrinidad.github.io/kanban-board-code-test/'
+    : '/'
 })
